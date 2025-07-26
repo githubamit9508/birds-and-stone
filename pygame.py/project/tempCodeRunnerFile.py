7cea804c5376211149155b5e84a9da7905801f18
Amit Nayak <amitn5201@gmail.com>
2025-07-26 12:10:02 +0530

    Add files via upload

---
+++ pygame.py/project/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+powerlines = []  # ✅ Define the list first
+level = []
+birds = [] 
+powerlines.append(powerlines(100, 275, 610, 275))
+powerlines.append(powerlines(30, 300, 610, 300))
+
+if level== 8:  # final level
+   birds.append(birds(320 - 31/2 - 175, 200 - 58, False, 0, 0, 0))
+   birds.append(birds(320 - 31/2 - 87.5, 200 - 58, False, 0, 0, 0))
+   birds.append(birds(320 - 31/2, 100 - 58, False, 0, 0, 0))
